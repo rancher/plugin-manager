c1932e19bf3b27e071c8fccd901763e71c993f7b
Darren Shepherd <darren@rancher.com>
2016-11-29 22:40:08 -0700

    Close handle

--- network/netns.go
+++ network/netns.go
@@ -10,10 +10,14 @@ func (s *state) hasNetwork(pid int) (bool, error) {
 	if err != nil {
 		return false, err
 	}
+	defer ns.Close()
+
 	handler, err := netlink.NewHandleAt(ns)
 	if err != nil {
 		return false, err
 	}
+	defer handler.Delete()
+
 	links, err := handler.LinkList()
 	return len(links) > 1, err
 }
