0e7a58eb932a286279583006b80a30a74420e2b3
Darren Shepherd <darren@rancher.com>
2016-12-28 00:26:27 -0700

    Don't make network-manager require working DNS anymore to start

--- package/start.sh
+++ package/start.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -e
 
-if [ -n "$METADATA_IP" ]; then
-    cat > /etc/resolv.conf << EOF
-search rancher.internal
-nameserver ${METADATA_IP}
-EOF
-fi
-
 for i in $(curl -s --unix /var/run/docker.sock http://localhost/info | jq -r .DockerRootDir) /var/lib/docker /run /var/run; do
     for m in $(tac /proc/mounts | awk '{print $2}' | grep ^${i}/); do
         umount $m || true
