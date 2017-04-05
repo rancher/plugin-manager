adb7d33bceba70a4825d60ca2b7634abc02f951f
leodotcloud <leodotcloud@gmail.com>
2017-04-04 18:44:15 -0700

    Fixing tests

--- arpsync/arpsync_test.go
+++ arpsync/arpsync_test.go
@@ -33,24 +33,3 @@ func TestDoSync(t *testing.T) {
 		logrus.Errorf("arpsync: error doing a sync of the ARP table: %v", err)
 	}
 }
-
-// For development purposes
-func TestArpWatcher(t *testing.T) {
-	if !inDevelopment {
-		t.Skip("not in development mode")
-	}
-	logrus.SetLevel(logrus.DebugLevel)
-	logrus.Debugf("TestArpWatcher")
-	mc, err := metadata.NewClientAndWait("http://169.254.169.250/2016-07-29")
-	if err != nil {
-		logrus.Errorf("error creating metadata client")
-		t.Fail()
-	}
-
-	atw := &ARPTableWatcher{
-		syncInterval: time.Duration(10) * time.Second,
-		mc:           mc,
-	}
-
-	atw.syncLoop()
-}
